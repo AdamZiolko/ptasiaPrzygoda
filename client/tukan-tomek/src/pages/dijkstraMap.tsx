a4d1a9042c864a096ff64054789a05232d0b3717
AdamZiolko <theadamzet@gmail.com>
2024-06-15 16:22:44 +0200

    node deletion mechanism, start of gradients

--- client/tukan-tomek/src/pages/dijkstraMap.tsx
+++ client/tukan-tomek/src/pages/dijkstraMap.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import paper from 'paper';
 import './dijkstraMap.css';
-import { Button, createMuiTheme, createTheme, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControlLabel, makeStyles, TextField, ThemeProvider } from '@material-ui/core';
+import { Button, createMuiTheme, createTheme, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControlLabel, IconButton, makeStyles, SvgIcon, TextField, ThemeProvider } from '@material-ui/core';
 import Switch from '@material-ui/core/Switch';
+import { useMediaQuery } from 'react-responsive';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const theme = createTheme({
   palette: {
@@ -16,11 +19,18 @@ const theme = createTheme({
 });
 
 const useStyles = makeStyles((theme) => ({
+  iconButton: {
+    '&:focus': {
+      outline: 'none',
+    },
+  },
   floatingButton: {
     position: 'fixed',
     bottom: theme.spacing(2),
   },
   primaryButton: {
+    height: theme.spacing(7),
+
     right: theme.spacing(2),
   },
   secondaryButton: {
@@ -29,9 +39,19 @@ const useStyles = makeStyles((theme) => ({
   },
   thirdButton: {
     color: 'black',
-    right: theme.spacing(45),
+    right: theme.spacing(14),
+    bottom: theme.spacing(6),
   },
-
+  trashButton: {
+    right: theme.spacing(46),
+    width: theme.spacing(7),
+    bottom: theme.spacing(3),
+    height: theme.spacing(7),
+    boxShadow: '0 0 10px 5px rgba(255, 0, 0, 0.5)', // Red glow
+    '& svg': { // Make the icon itself bigger
+      fontSize: '2rem',
+    },
+  },
 
   fpsDisplay: {
     top: theme.spacing(5),
@@ -55,9 +75,28 @@ export function DijkstraMap() {
   const [editedNode, setEditedNode] = useState(null);
   const [open, setOpen] = useState(false);
   const [dialogContent, setDialogContent] = useState({ nodeName: '', nodeConnections: '' });
+  const [nodeToDeletion, setNodeToDeletion] = useState(null);
+  const [trashIsClicked, setTrashIsClicked] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
+
+  const handleTrashClick = () => {
+    if (switchEdit) setSwitchEdit(!switchEdit)
+    if (switchState) setSwitchState(!switchState)
+
+    setTrashIsClicked(prevTrashIsClicked => {
+      const newTrashIsClicked = !prevTrashIsClicked;
+      console.log(newTrashIsClicked, 'trash button clicked');
+      if (newTrashIsClicked) {
+        setSnackbarOpen(true); // Open the snackbar when trash mode is enabled
+      }
+      return newTrashIsClicked;
+    });
+  };
 
   const changeSwitchState = () => {
+    if (switchEdit) setSwitchEdit(!switchEdit)
+    if (trashIsClicked) setTrashIsClicked(!trashIsClicked)
     setSwitchState(prevSwitchState => {
       const newSwitchState = !prevSwitchState;
       console.log(newSwitchState, 'czy wlaczany');
@@ -66,6 +105,9 @@ export function DijkstraMap() {
   };
 
   const changeSwitchEdit = () => {
+    if (switchState) setSwitchState(!switchState)
+    if (trashIsClicked) setTrashIsClicked(!trashIsClicked)
+
     setSwitchEdit(prevSwitchEdit => {
       const newSwitchEdit = !prevSwitchEdit;
       console.log(newSwitchEdit, 'czy wlaczany edytcja');
@@ -123,29 +165,29 @@ export function DijkstraMap() {
     }));
   };
 
-const addEdge = () => {
-  if (selectedNodes.length === 2) {
-    setGraph(prevGraph => {
-      // Create a deep copy of prevGraph
-      const newGraph = JSON.parse(JSON.stringify(prevGraph));
+  const addEdge = () => {
+    if (selectedNodes.length === 2) {
+      setGraph(prevGraph => {
+        // Create a deep copy of prevGraph
+        const newGraph = JSON.parse(JSON.stringify(prevGraph));
 
-      if (newGraph[selectedNodes[0]]?.[selectedNodes[1]]) {
-        console.log('Before deletion:', newGraph);
-        delete newGraph[selectedNodes[0]][selectedNodes[1]];
-        delete newGraph[selectedNodes[1]][selectedNodes[0]];
-        console.log('After deletion:', newGraph);
-        return newGraph;
-      } else {
-        return {
-          ...newGraph,
-          [selectedNodes[0]]: { ...newGraph[selectedNodes[0]], [selectedNodes[1]]: 1 },
-          [selectedNodes[1]]: { ...newGraph[selectedNodes[1]], [selectedNodes[0]]: 1 }
-        };
-      }
-    });
-    setSelectedNodes([]);
-  }
-};
+        if (newGraph[selectedNodes[0]]?.[selectedNodes[1]]) {
+          console.log('Before deletion:', newGraph);
+          delete newGraph[selectedNodes[0]][selectedNodes[1]];
+          delete newGraph[selectedNodes[1]][selectedNodes[0]];
+          console.log('After deletion:', newGraph);
+          return newGraph;
+        } else {
+          return {
+            ...newGraph,
+            [selectedNodes[0]]: { ...newGraph[selectedNodes[0]], [selectedNodes[1]]: 1 },
+            [selectedNodes[1]]: { ...newGraph[selectedNodes[1]], [selectedNodes[0]]: 1 }
+          };
+        }
+      });
+      setSelectedNodes([]);
+    }
+  };
 
   let circles: paper.Path.Circle[] = [];
 
@@ -181,11 +223,47 @@ const addEdge = () => {
           );
           nodePositions[node] = position;
 
-          const circle = new paper.Path.Circle({
-            center: position,
-            radius: 30,
-            fillColor: selectedNodes.includes(node) ? 'blue' : 'red'
-          });
+          const keys = Object.keys(graph);
+          const asciiValues = keys.map(key => key.charCodeAt(0));
+          const maxAscii = Math.max(...asciiValues);
+          const minAscii = Math.min(...asciiValues);
+          const highestLetter = String.fromCharCode(maxAscii);
+          const lowestLetter = String.fromCharCode(minAscii);
+
+          let circle;
+          if (node === highestLetter || node === lowestLetter) {
+            let size = new paper.Size(30 * 2, 30 * 2);
+            let topLeftPosition = new paper.Point(position.x - size.width / 2, position.y - size.height / 2);
+        
+            circle = new paper.Path.Rectangle({
+                rectangle: new paper.Rectangle(topLeftPosition, size),
+            });
+        
+            // Create a linear gradient that runs from the top-left to the bottom-right of the rectangle
+            let gradient = new paper.Gradient(['#FFA07A', '#FFD700'], true);
+            let gradientColor = new paper.Color(gradient, topLeftPosition, topLeftPosition.add(size));
+            circle.fillColor = gradientColor;
+        
+            // Add a shadow to the rectangle
+            circle.shadowColor = new paper.Color(0, 0, 0);
+            circle.shadowBlur = 12;
+            circle.shadowOffset = new paper.Point(5, 5);
+        } else {
+            circle = new paper.Path.Circle({
+                center: position,
+                radius: 30,
+            });
+        
+            // Create a radial gradient that runs from the center to the edge of the circle
+            let gradient = new paper.Gradient(['#FFA07A', '#FFD700'], false);
+            let gradientColor = new paper.Color(gradient, position, position.add([30, 30]));
+            circle.fillColor = gradientColor;
+        
+            // Add a shadow to the circle
+            circle.shadowColor = new paper.Color(0, 0, 0);
+            circle.shadowBlur = 12;
+            circle.shadowOffset = new paper.Point(5, 5);
+        }
           circles.push(circle);
 
           let isDragging = false;
@@ -220,10 +298,38 @@ const addEdge = () => {
               console.log("jestem tutaj");
               console.log('switchEdit', switchEdit);
               // stan switch edit w use effekcje
+
+              if (trashIsClicked) {
+                const newGraph = { ...graph };
+
+                // Iterate over all nodes in the graph
+                for (let otherNode in newGraph) {
+                  // If the other node has a connection to the node to be deleted, remove it
+                  if (newGraph[otherNode][node]) {
+                    delete newGraph[otherNode][node];
+                  }
+                }
+
+                // Delete the node itself
+                delete newGraph[node];
+
+                // Update the graph state
+                setGraph(newGraph);
+                return;
+              }
+
               if (switchEdit) {
                 setEditedNode(node);
+
+                
+
                 changeAllCirclesColor('red');
-                this.fillColor = 'purple';
+
+
+                let gradient = new paper.Gradient(['#FFA07A', '#9370DB'], false);
+                let gradientColor = new paper.Color(gradient, position, position.add([30, 30]));
+                this.fillColor = gradientColor;
+                
                 console.log("jestem tutaj");
                 console.log("Node name: ", node);
                 console.log("Node connections: ", graph[node]);
@@ -271,6 +377,20 @@ const addEdge = () => {
           edgePaths.length = 0;
           let addedTexts = {}; // Object to keep track of added texts
           Object.keys(graph).forEach(node => {
+            // Create a PointText for the node
+            const nodeText = new paper.PointText({
+              point: new paper.Point(
+                node === movedNode ? newPosition.x : nodePositions[node].x,
+                (node === movedNode ? newPosition.y : nodePositions[node].y) + 12 // Adjust the y-coordinate
+              ),
+              content: node.toUpperCase(), // Use the node as the content, converted to uppercase
+              fillColor: 'black',
+              justification: 'center',
+              fontSize: 24, // Increase font size
+              fontWeight: 'bold' // Make the text bold
+            });
+            edgePaths.push(nodeText);  // Add the node text to the edgePaths array
+
             Object.keys(graph[node]).forEach(neighbor => {
               const path = new paper.Path.Line({
                 from: node === movedNode ? newPosition : nodePositions[node],
@@ -308,7 +428,7 @@ const addEdge = () => {
         window.removeEventListener('resize', handleResize);
       };
     }
-  }, [graph, selectedNodes, switchState, switchEdit]);
+  }, [graph, selectedNodes, switchState, switchEdit, trashIsClicked]);
 
 
   useEffect(() => {
@@ -322,7 +442,7 @@ const addEdge = () => {
     <ThemeProvider theme={theme}>
       <div className="canvas-container">
         <canvas ref={canvasRef} className="full-page-canvas" />
-        <Button variant="contained" color="primary" onClick={addNode} className={`${classes.floatingButton} ${classes.primaryButton}`}>wezel</Button>
+        <Button variant="contained" color="primary" onClick={addNode} className={`${classes.floatingButton} ${classes.primaryButton}`}>+ węzeł</Button>
 
 
         <FormControlLabel
@@ -358,6 +478,12 @@ const addEdge = () => {
 
 
         <div className={` ${classes.fpsDisplay}`}>FPS: {fps}</div>
+        <IconButton onClick={handleTrashClick} style={{ color: trashIsClicked ? 'red' : 'black' }} className={`${classes.floatingButton} ${classes.trashButton} ${classes.iconButton}`} >
+          <SvgIcon>
+            <path d="M3 6l3 18h12l3-18h-18zm19-4v2h-20v-2h5.5l1-1h7l1 1h5.5z" />
+          </SvgIcon>
+        </IconButton>
+
 
         <div>
           {/* ... */}
@@ -434,6 +560,16 @@ const addEdge = () => {
         </div>
 
       </div>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <MuiAlert onClose={() => setSnackbarOpen(false)} severity="error">
+          Tryb usuwania włączony! Kliknij na węzeł, aby go usunąć.
+        </MuiAlert>
+      </Snackbar>
     </ThemeProvider>
   );
 }
